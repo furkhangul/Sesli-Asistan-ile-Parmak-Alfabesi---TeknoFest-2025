dd0394e667f68307a29130490b21e7cae3788f04
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-03-29 18:30:11 +0300

    Update main.py

--- Project VOL I/main.py
+++ Project VOL I/main.py
@@ -1,12 +1,11 @@
-import speech_recognition as sp  # Sesin text formatına çevirmek için kullanacağız.
+import speech_recognition as sp 
 
 def ses_tanima():
-    tanimlayici = sp.Recognizer()  # Seslerin yazıya dönüştürülmesi için tanımlamasını sağlar.
+    tanimlayici = sp.Recognizer()
     with sp.Microphone() as mic:
         print("Sesli Komut Bekleniyor. (Bir cümle söyleyin)")
-        ses = tanimlayici.listen(mic)  # Mikrofonu dinlemeye başlar.
+        ses = tanimlayici.listen(mic) 
         try:
-            # Google API'si ile sesli komutları tanıyacağız ve dil parametresi olarak Türkçe'yi belirtiyoruz.
             komut = tanimlayici.recognize_google(ses, language="tr")
             print(f"Ses: {komut}")
         except sp.UnknownValueError:
@@ -15,4 +14,4 @@ def ses_tanima():
             print("API ile bağlantı sağlanamadı!")
 
 if __name__ == "__main__":
-    ses_tanima()  # Sesli komutları tanımaya başlıyoruz.
+    ses_tanima()
